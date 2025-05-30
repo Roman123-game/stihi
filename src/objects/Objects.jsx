3209e8483fce96d16e112e83156d7ae948cac0f2
mrr <broitman.rom@gmail.com>
2025-05-30 17:33:54 +0300

    Initialize React project with Vite, ESLint configuration, and basic structure. Added .gitignore, package.json, and essential files including App component, styles, and README. Set up ESLint rules and Vite configuration for development.

---
+++ src/objects/Objects.jsx
@@ -0,0 +1,17 @@
+
+export default Objects = [
+    {
+      id: 1,
+      startDate: "2024-06-01",
+      title: "First Event",
+      author: "This is the first event."
+    },
+    {
+      id: 2,
+      startDate: "2024-07-01",
+      title: "Second Event",
+      author: "This is the second event."
+    }
+  ];
+
+
