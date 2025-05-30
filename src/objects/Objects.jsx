8f2490d8a1e32b99f117e9ca5d9d26e622356fa2
mrr <broitman.rom@gmail.com>
2025-05-30 18:29:31 +0300

    update Objects.jsx

--- src/objects/Objects.jsx
+++ src/objects/Objects.jsx
@@ -5,7 +5,7 @@ const Objects = [
     id: 1,
     title: "Пепел",
     text: "Я пепел в памяти огня,\n Любовь сгорает без тебя,\n Ты ветер вечно не в судьбе,\n Играешь пеплом в пустоте.",
-    author: "Julia Movchan"
+    author: "Julia"
   },
   {
     id: 2,
