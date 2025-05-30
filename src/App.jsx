16d49bc7b3fc164b4da2c36f8a331bd3b2e251c6
mrr <broitman.rom@gmail.com>
2025-05-30 17:50:12 +0300

    update App.css, App.jsx and Objects.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Fragment } from 'react'
 import './App.css'
+import Objects from './objects/Objects';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  // Example array of objects
-  const objects = [
-    { id: 1, title: 'Card One', description: 'This is the first card.' },
-    { id: 2, title: 'Card Two', description: 'This is the second card.' },
-    { id: 3, title: 'Card Three', description: 'This is the third card.' },
-  ];
 
   return (
-    <>
-  
+    <Fragment>
       <h2>Poem</h2>
-      <div style={{ display: 'flex', gap: '1rem', flexWrap: 'wrap' }}>
-        {objects.map(obj => (
-          <div key={obj.id} style={{ border: '1px solid #ccc', borderRadius: '8px', padding: '1rem', minWidth: '200px', boxShadow: '2px 2px 8px #eee' }}>
+      <div className='mainCont'>
+        {Objects.map(obj => (
+          <div className='secondCont' key={obj.id} >
             <h3>{obj.title}</h3>
             <p>{obj.text}</p>
             <p>{obj.author}</p>
           </div>
         ))}
       </div>
-    </>
+    </Fragment>
   )
 }
 
